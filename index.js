993e5accd61c9ebf42d0f20894d84e8a3954914f
Luna <boreum4151@gmail.com>
2021-07-06 22:41:12 +0900

    update index.js

--- index.js
+++ index.js
@@ -1,32 +1,105 @@
-const axios = require("axios").default;
-require("dotenv").config();
+const request = require("request-promise");
+const Excels = require("exceljs");
+
+const getSingleUserCoupon = async (qs, authorization) => {
+  try {
+    const output = await request.get({
+      url: "https://www.weport.co.kr/v2/coupon/user_coupon/",
+      headers: {
+        authorization,
+        "content-type": "application/json",
+      },
+      qs,
+      json: true,
+    });
+    return output;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+async function processArray(array, qs, authorization) {
+  let result = [];
+  for (const i of array) {
+    const output = await getSingleUserCoupon(
+      { ...qs, offset: 100 * (i + 1) },
+      authorization
+    );
+    result = result.concat(output.results);
+  }
+  return result;
+}
+
+const getTotalUserList = async () => {
+  try {
+    const authorization = "Bearer InwhT8NflNweA994zynThE4hJwNjpj";
+    const qs = {
+      coupon_id: 14671,
+      offset: 0,
+      limit: 100,
+    };
+
+    let totalList = [];
+
+    const offset0 = await getSingleUserCoupon(qs, authorization);
+    const totalCount = offset0.count;
+    if (offset0.next === null) {
+      totalList = totalList.concat(offset0.results);
+
+      const filtered_used_list = totalList.reduce(
+        (pre, cur) => (cur.is_used ? [...pre, cur] : pre),
+        []
+      );
+      const result = filtered_used_list.map(({ user_info }) => [
+        user_info.name,
+        user_info.phone,
+        user_info.email,
+      ]);
+      return result;
+    }
+
+    totalList = totalList.concat(offset0.results);
+
+    const callLimit = Math.floor(totalCount / 100);
+
+    const indexArray = Array.from({ length: callLimit }, (_, i) => i);
+
+    const lastUserList = await processArray(indexArray, qs, authorization);
+
+    totalList = totalList.concat(lastUserList);
+
+    const filtered_used_list = totalList.reduce(
+      (pre, cur) => (cur.is_used ? [...pre, cur] : pre),
+      []
+    );
+    const result = filtered_used_list.map(({ user_info }) => [
+      user_info.name,
+      user_info.phone,
+      user_info.email,
+    ]);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 (async () => {
   try {
-    const url = process.env.URL;
-    const body = {
-      operationName: "BoardScreen",
-      variables: {
-        boardSlug: "testreview",
-        page: 1,
-        field: "title",
-        word: "",
-        boardHeaderID: "",
-      },
-      query:
-        "query BoardScreen($boardSlug: String!, $page: Int!, $field: String, $word: String, $boardHeaderID: ID) {\n  boardBySlug(boardSlug: $boardSlug) {\n    id\n    ...BoardList_board\n    __typename\n  }\n}\n\nfragment BoardList_board on Board {\n  id\n  title\n  subtitle\n  notices {\n    ...BoardListItem_post\n    __typename\n  }\n  subNotices {\n    ...BoardListItem_post\n    __typename\n  }\n  extension {\n    isFAQ\n    service\n    __typename\n  }\n  posts(filterBy: {field: $field, word: $word, boardHeaderID: $boardHeaderID}, pagination: {page: $page, pageSize: 20}, orderBy: {field: CREATED_AT, direction: DESC}) {\n    nodes {\n      id\n      ...BoardListItem_post\n      __typename\n    }\n    totalCount\n    __typename\n  }\n  __typename\n}\n\nfragment BoardListItem_post on Post {\n  id\n  title\n  commentCount\n  createdAt\n  views\n  likes\n  nickname\n  imageSrc\n  youtubeSrc\n  board {\n    id\n    slug\n    title\n    __typename\n  }\n  author {\n    id\n    username\n    nickname\n    __typename\n  }\n  attachments {\n    id\n    __typename\n  }\n  boardHeader {\n    id\n    name\n    __typename\n  }\n  __typename\n}\n",
-    };
+    const getTotalUser = await getTotalUserList();
 
-    // console.log(process.env.LIST_QUERY)
-    const response = await axios.post(url, body, {
-      headers: {
-        "content-type": "application/json",
-        "content-length": JSON.stringify(body).toString().length,
-      },
-    });
+    // 관리 객체 생성
+    const workbook = new Excels.Workbook();
 
-    console.log(response.data);
-  } catch (error) {
-    console.log("error ::::: ", error);
+    // 엑셀 시트 생성
+    const sheet = workbook.addWorksheet("first sheet");
+
+    sheet.addRows(getTotalUser);
+
+    workbook.xlsx
+      .writeFile("./first_excel.xlsx")
+      .then((_) => console.log("완료"))
+      .catch((_) => console.log("실패"));
+  } catch (err) {
+    console.log(err);
   }
 })();
